0cc5dfdb18a85df01b4fe458812e68acd17ea288
Abe <91988972+AbeFernandez@users.noreply.github.com>
2022-06-30 11:14:22 -0700

    update

--- week1B/PracticeInClass/fileDemo.py
+++ week1B/PracticeInClass/fileDemo.py
@@ -1,6 +1,21 @@
 # Practice in class: File Demo
 import os
+
+# Create a file
 myFile = open("myFile.txt", "x")
+
+# Write in a file
 myFile = open("myFile.txt", "w")
+myFile.write("Hello World")
+
+# Append to a file
 myFile = open("myFile.txt", "a")
-os.remove("myFile.txt")
+myFile.write("\nHello, again")
+myFile.close()
+
+# Remove the file
+if os.path.exists("myFile.txt"):
+    os.remove("myFile.txt")
+else:
+    print("File don't exist!")
+
