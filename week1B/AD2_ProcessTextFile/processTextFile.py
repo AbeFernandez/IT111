4625f972da3d64748576c32052262cc8719e391b
Abe <91988972+AbeFernandez@users.noreply.github.com>
2022-07-04 18:58:47 -0700

    Update

---
+++ week1B/AD2_ProcessTextFile/processTextFile.py
@@ -0,0 +1,13 @@
+# Opens a file for append
+myFile = open("Project2Input.txt", "a")
+myFile.write("\n")
+
+# The user add text to the file through command line input
+print("Hello! You can use this program to edit a file and save a copy.")
+userInput = input("Add a text to the file 'Project2Input.txt': ")
+myFile.write(userInput)
+print("Your text have been added.")
+userFileCopy =
+# Saves a copy of the file with a different file name
+
+# Deletes the original file
